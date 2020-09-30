cce9b55b9adab037b25526c244c2b253e4bb9a02
Steven Tardiff <tardiff.steven@gmail.com>
2020-09-30 13:24:21 -0400

    add middle and auth

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //connect Database
 connectDB();
 
+// init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 // define routes
