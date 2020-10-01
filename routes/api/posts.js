d54442e6897a76987b28ac3a9ba60458b07f39de
Steven Tardiff <tardiff.steven@gmail.com>
2020-10-01 11:56:45 -0400

    delete comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -185,4 +185,41 @@ router.post(
   }
 );
 
+// @route       DELETE api/posts/comment/:id/:comment_id
+// @desc        Delete a comment on a post
+// @access      Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    //Pull comment from post
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    //make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment not found' });
+    }
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    //check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    //get remove index
+    const removeIndex = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Errror');
+  }
+});
 module.exports = router;
