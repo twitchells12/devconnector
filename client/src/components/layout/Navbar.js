0390b4eea6cf24d82d26c7e05011aebab0d6ccf2
Steven Tardiff <tardiff.steven@gmail.com>
2020-10-01 15:22:55 -0400

    added actions reducers

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,7 +6,7 @@ export const Navbar = () => {
     <nav className="navbar bg-dark">
       <h1>
         <Link to="/">
-          <i className="fas fa-code"></i> DevConnector
+          <i className="fa fa-code"></i> DevConnector
         </Link>
       </h1>
       <ul>
