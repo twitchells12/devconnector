38427aecb274346918873e7ae038bb594165899d
Steven Tardiff <tardiff.steven@gmail.com>
2020-10-06 16:19:14 -0400

    add Education

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -88,7 +88,7 @@ export const addExperience = (formData, history) => async (dispatch) => {
 };
 
 // Add education
-export const addEdcuation = (formData, history) => async (dispatch) => {
+export const addEducation = (formData, history) => async (dispatch) => {
   try {
     const config = {
       headers: {
