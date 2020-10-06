adc7968e3d19599e4fcd27ea3fbf1b816b831a0c
Steven Tardiff <tardiff.steven@gmail.com>
2020-10-06 14:04:58 -0400

    add profile

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -3,7 +3,6 @@ import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { login } from '../../actions/auth';
-import { ptBR } from 'date-fns/esm/locale';
 
 const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData] = useState({
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { ptBR } from 'date-fns/esm/locale';
 
 const Landing = ({ isAuthenticated }) => {
   if (isAuthenticated) {
