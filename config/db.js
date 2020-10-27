e3cbf4171460610e6e626737a5a8c44d40bcbbbe
Steven Tardiff <tardiff.steven@gmail.com>
2020-10-27 16:55:49 -0400

    dbchange

--- config/db.js
+++ config/db.js
@@ -1,21 +1,19 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
       useFindAndModify: false,
     });
-    console.log('MongoDB connected');
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error(err.message);
+    console.error(`Error: ${error.message}`.red.underline.bold);
     //exit process with failure
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
